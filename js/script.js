a8f816a4e635a6e694f37f75baba0d91e0190c62
dagdPTC <20240167@ricaldone.edu.sv>
2025-06-05 11:24:59 -0600

    Funcionalidad casi completa (agregar)

--- js/script.js
+++ js/script.js
@@ -45,3 +45,57 @@ ObtenerRegistros();
 //Proceso para agregar registros 
 const modal = document.getElementById("mdAgregar");
 const btnAgregar = document.getElementById("btnAgregar");
+const btnCerrar = document.getElementById("btnCerrarModal")
+
+btnAgregar.addEventListener("click", ()=>{
+    modal.showModal(); //Abre el botón cuando a btnAgregar se le hace el click
+});
+
+btnCerrar.addEventListener("click", ()=>{
+    modal.close(); //Abre el botón cuando a btnAgregar se le hace el click
+});
+
+//Agregar un nuevo integrante desde el formulario
+document.getElementById("frmAgregar").addEventListener("submit", async e => {
+    e.preventDefault(); //Evita que los datos de envíen por defecto
+
+    //Capturar los valores del formulario
+    const nombre = document.getElementById("txtNombre").value.trim();
+    const apellido = document.getElementById("txtApellido").value.trim();
+    const correo = document.getElementById("txtEmail").value.trim();
+
+    //Validación básica
+    if(!nombre || !apellido || !correo){
+        alert("Complete todos los campos");
+        return; //Evita que el código se siga ejecutando
+    }
+
+    //Llamar a la API para enviar los datos
+    const respuesta = await fetch(API_URL, {
+        method: "POST", 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({nombre,apellido,correo})
+    });
+
+    if(respuesta.ok){
+        //Mensaje de confirmación
+        alert("El registro fue agregado correctamente");
+
+        //Limpiar el formulario
+        document.getElementById("frmAgregar").reset();
+
+        //Cerrar el modal (dialog)
+        modal.close();
+
+        //Recarga la tabla
+        ObtenerRegistros();
+    }
+    else{
+        alert("Error, no se pudo guardar");
+    }
+
+})
+
+
+
+
