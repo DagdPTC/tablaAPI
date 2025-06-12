ae98af24ef8361bcc0f4ca50032676d5945bd4dc
dario <20240167@ricaldone.edu.sv>
2025-06-12 10:24:33 -0600

    odal de Editar

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ function MostrarRegistros(datos){
                 <td>${persona.apellido}</td>
                 <td>${persona.correo}</td>
                 <td>
-                    <button>Editar</button>
+                    <button onclick="AbrirModalEditar('${persona.id}','${persona.nombre}','${persona.apellido}','${persona.correo}')">Editar</button>
                     <button onclick="EliminarPersona(${persona.id})">Eliminar</button>
                 </td>
             </tr>
@@ -111,6 +111,27 @@ async function EliminarPersona(id){
     }
 }
 
+
+
+
+/*Funcionalidad para editar registros*/
+const modalEditar = document.getElementById("mdEditar");
+const btnCerrarEditar = document.getElementById("btnCerrarEditar");
+
+btnCerrarEditar.addEventListener("click", ()=>{
+    modalEditar.close(); //Cerrar Modal de editar
+});
+
+function AbrirModalEditar(id, nombre, apellido, correo){
+    //Agregamos los valores a los input antes de abrir el modal
+    document.getElementById("txtIdEditar").value = id;
+    document.getElementById("txtNombreEditar").value = nombre;
+    document.getElementById("txtApellidoEditar").value = apellido;
+    document.getElementById("txtEmailEditar").value = correo;
+
+    //Modal se abre después de agregar los valores a los input
+    modalEditar.showModal();
+}
 
 
 
